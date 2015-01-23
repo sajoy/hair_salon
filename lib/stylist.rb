914ebf936b69a0d6d8ad2c8d0f0b425737552fc5
Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-01-23 09:59:54 -0800

    add Stylist#all, Stylist#save, Stylist#==

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -7,7 +7,25 @@ class Stylist
     @id = attributes[:id]
   end
 
+  define_method(:save) do
+    result = DB.exec("INSERT INTO stylists (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
 
+  define_singleton_method(:all) do
+    all_stylists = []
+    data = DB.exec("SELECT * FROM stylists;")
+    data.each() do |stylist|
+      name = stylist["name"]
+      id = stylist["id"]
+      all_stylists.push(Stylist.new({:name => name, :id => id}))
+    end
+    all_stylists
+  end
+
+  define_method(:==) do |other_stylist|
+    name().==(other_stylist.name())
+  end
 
 
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -5,6 +5,7 @@ describe(Stylist) do
   describe('#id') do
     it("will return a stylist's unique id as a fixnum") do
       stylist1 = Stylist.new({:name => "Holly W.", :id => nil})
+      stylist1.save()
       expect(stylist1.id()).to(be_an_instance_of(Fixnum))
     end
   end
@@ -13,6 +14,38 @@ describe(Stylist) do
     it("will return a stylist's name") do
       stylist1 = Stylist.new({:name => "Jackson H.", :id => nil})
       expect(stylist1.name()).to(eq("Jackson H."))
+    end
+  end
+
+  describe("#save") do
+    it("will save a new Stylist to the hair_salon database") do
+      stylist1 = Stylist.new({:name => "Avril L.", :id => nil})
+      stylist1.save()
+      expect(Stylist.all()).to(eq([stylist1]))
+    end
+  end
+
+  describe(".all") do
+    it("will start empty") do
+      expect(Stylist.all()).to(eq([]))
+    end
+
+    it("will return an array of all saved Stylists") do
+      stylist1 = Stylist.new({:name => "Holly W.", :id => nil})
+      stylist1.save()
+      stylist2 = Stylist.new({:name => "Jackson H", :id => nil})
+      stylist2.save()
+      stylist3 = Stylist.new({:name => "Avril L.", :id => nil})
+      stylist3.save()
+      expect(Stylist.all()).to(eq([stylist1, stylist2, stylist3]))
+    end
+  end
+
+  describe("#==") do
+    it("will say two Stylists are equal if their names are the same") do
+      stylist1 = Stylist.new({:name => "Avril L.", :id => nil})
+      stylist2 = Stylist.new({:name => "Avril L.", :id => nil})
+      expect(stylist1).to(eq(stylist2))
     end
   end
 
